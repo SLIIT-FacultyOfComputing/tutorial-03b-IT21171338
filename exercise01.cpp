b4dc8e968438e320deb8b2f159e1568a373de135
IT21171338 <99181984+IT21171338@users.noreply.github.com>
2022-02-21 14:57:07 +0000

    I have exercise1,3

--- exercise01.cpp
+++ exercise01.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <iomanip>
+
 using namespace std;
 int main() {
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout << "No\t" << "Name\t\t\t" << "Marks\t" << endl;
+ cout << "No\t" << "Name\t\t" << "Marks\t" << endl;
  for (int r = 0; r < 5; r++) {
-     cout <<  r+1 
-          <<  "\t" << names[r]<<set (15)<<marks[r] << endl;
+     cout <<  r+1 <<  "\t" << names[r]<<"\t"<<marks[r] << endl;
  }
 }
--- exercise04.cpp
+++ exercise04.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int &len, int &wth);
 
 // Do not change the main() function
 int main() {
@@ -17,5 +17,21 @@ void print(int len, int wth) {
    cout << "Length : " << len 
         << ", Width  : " << wth << endl;
 }
+void input(int &len, int &wth) {
+  cout <<"Enter  length:";
+  cin >> len ;
+  cout <<"Enter  width:";
+  cin >> wth ;
+
+
+}
 
 // Implement the Input Function here
+/*1. Do you get the correct values printed ?
+    ans: No, It prints the assigned values in the main function
+  2.Modify the parameters of your input function as given below, to use length and width as reference type parameters.
+      void input(int &length, int &width);
+
+  3.Do you get the correct values printed ?
+    ans:Yes
+*/
