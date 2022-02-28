529da603fc3a71ed87a9e526aa3dcf4e548782ba
IT21171338 <99181984+IT21171338@users.noreply.github.com>
2022-02-28 05:29:00 +0000

    I edited ex01

--- exercise01.cpp
+++ exercise01.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main() {
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
- char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
+ string names[] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
  cout << "No\t" <<setw(12)<< "Name\t\t\t" << "Marks\t" << endl;
  for (int r = 0; r < 5; r++) {
      cout <<  r+1 <<  setw(12) << names[r]<<setw(15)<<marks[r] << endl;
